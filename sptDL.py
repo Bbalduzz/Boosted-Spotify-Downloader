039b514e64dee9478abe5851186f16e1abae5dc6
Edoardo Balducci <81587335+Bbalduzz@users.noreply.github.com>
2022-11-11 19:25:36 +0100

    Update sptDL.py

--- sptDL.py
+++ sptDL.py
@@ -10,6 +10,8 @@ from yt_dlp import YoutubeDL
 import urllib.request
 import ui
 
+import multiprocessing
+
 client_credentials_manager = SpotifyClientCredentials('1a347b168ca34b2c967e85e733f164c6', 'c6e6ef117c3b426facabb106683cc0bb')
 sp = spotipy.Spotify(client_credentials_manager = client_credentials_manager)
 
@@ -41,41 +43,20 @@ def progress_bar(progress, total):
     percent = 100 * (progress / float(total))
     bar = '█' * int(percent) + '-' * (100 - int(percent))
     print(f'\rDownloading track... |{bar}| {percent:.2f}%', end='\r')
-
 def my_hook(d):
     if d['status'] == 'downloading':
-    	progress_bar(round(float(d['downloaded_bytes'])), float(d['total_bytes']))
-    if d['status'] == 'finished':
-        c.print('\r ==> [yellow]Downloaded now converting[/yellow]')
+    	print('')
 
-def download_track(track):
+def get_links(track):
 	try:
 		track_name = track['name']
 		track_arists = track['artist']
 		BASE_URL = f'https://www.youtube.com/results?search_query={track_name}+{track_arists}'.replace(" ", "%20")
 		video_ids = re.findall(r"watch\?v=(\S{11})", urllib.request.urlopen(BASE_URL).read().decode())
 		yt_url = "https://www.youtube.com/watch?v=" + video_ids[0]
-		c.print('[b green]Track found[/b green]: ', track_name, '-', track_arists)
+		return yt_url
 	except Exception as e:
 		c.print('[red]ERROR[/red]', e, '==> [red]Failed[/red]')
-		pass
-	try:
-		options = {
-			'writethumbnail':True,
-		    'format':'bestaudio/best',
-		    'postprocessors': [
-		    	{'key': 'FFmpegExtractAudio',
-		        'preferredcodec': 'mp3',
-		        'preferredquality': '320'},
-		        {'key': 'EmbedThumbnail'}],
-	        'quiet': True,
-		    'no_warnings': True,
-		   	'outtmpl':f'{playlist_name}/{track_name}.%(ext)s',
-		    'progress_hooks': [my_hook],
-	    }
-		with YoutubeDL(options) as ydl: 
-			ydl.download([yt_url])
-	except:
 		pass
 
 def apply_metadata(track, playlist_name):
@@ -93,6 +74,35 @@ def apply_metadata(track, playlist_name):
 	except:
 		pass
 
+def download_track(track, playlist_name, link):
+	track_name = track['name']
+	track_arists = track['artist']
+	c.print('[b green]Track found[/b green]: ', track_name, '-', track_arists)
+	try:
+		options = {
+			'writethumbnail':True,
+		    'format':'bestaudio/best',
+		    'postprocessors': [
+		    	{'key': 'FFmpegExtractAudio',
+		        'preferredcodec': 'mp3',
+		        'preferredquality': '320'},
+		        {'key': 'EmbedThumbnail'}],
+	        'quiet': True,
+		    'no_warnings': True,
+		   	'outtmpl':f'{playlist_name}/{track_name}.%(ext)s',
+		    'progress_hooks': [my_hook],
+	    }
+		with YoutubeDL(options) as ydl: 
+			ydl.download([link])
+	except:
+		pass
+
+def process_starter(track_id, playlist_name):
+	track = get_tracks_data(track_id)
+	link = get_links(track)
+	download_track(track, playlist_name, link)
+	apply_metadata(track, playlist_name)
+
 if __name__ == '__main__':
 	try:
 		c.print(ui.print_banner())
@@ -106,11 +116,12 @@ if __name__ == '__main__':
 		c.print(f'[b red]{playlist_name}[/b red]', 'by', f'[b red]{playlist_owner}', justify='center')
 		c.print('Tracks:',len(track_ids), justify='center')
 
-		for i in range(len(track_ids)):
-			track = get_tracks_data(track_ids[i])
-			print(f'[{i+1}/{len(track_ids)}]', end=' ')
-			download_track(track)
-			apply_metadata(track, playlist_name)
+		jobs = []
+		for i in track_ids:
+			downloader = multiprocessing.Process(target=process_starter, args=(i,playlist_name))
+			jobs.append(downloader)
+			downloader.start()
+
 	except KeyboardInterrupt:
 		c.print(ui.leave_msg())
 		exit()
